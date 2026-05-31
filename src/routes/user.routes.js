a267a338599f0b64ba282a7daa3e34049696c2a9
arushiiiiii <garushi850@gmail.com>
2026-05-31 21:04:50 +0530

    Add user account management routes: change password, get current user, update account details, update avatar, update cover image, user channel profile, and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, refreshAccessToken, registerUser, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -24,5 +24,15 @@ router.route("/login").post(loginUser)
 //secured routes - because user must be logged in here
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)   // patch kyunki hume sirf kuch fields update krne h
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/update-cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
+
+// kyunki ab hum params use kr rhe h
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)  // c stands for channel, isse url thoda clean rhega, aur easily samajh aa jayega ki ye route kis cheez se related h, baaki ishe format mein likhna hoga, c change ho skta h
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 
 export default router
